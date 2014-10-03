3c5821c3ba0eb847808496359addd5021828cb1d
clnoll <noll.catherine@gmail.com>
2014-10-03 11:43:49 -0700

    window resizing

--- public/js/application.js
+++ public/js/application.js
@@ -424,4 +424,30 @@ app.controller('allDAUCtrl', ['$scope',
                 }); // closes scope.watch
         });
     }
-]);
+])
+.directive('resizeable', function($window, $scope) {
+    $scope.initializeWindowSize = function() {
+        $scope.windowHeight = $window.innerHeight;
+        $scope.windowWidth = $window.innerWidth;
+    }
+
+    $scope.initializeWindowSize();
+    angular.element($window).bind('resize', function() {
+        $scope.initializeWindowSize();
+        $scope.$apply();
+    })
+})
+
+
+
+// App.directive 'resizable', ($window) ->
+//   ($scope) ->
+//     $scope.initializeWindowSize = ->
+//       $scope.windowHeight = $window.innerHeight
+//       $scope.windowWidth  = $window.innerWidth
+
+//     $scope.initializeWindowSize()
+
+//     angular.element($window).bind 'resize', ->
+//       $scope.initializeWindowSize()
+//       $scope.$apply()
